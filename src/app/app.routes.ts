7e344e33d515f731213ecfdd454b9551d24d8b96
Ítalo Gregório <italo-07@hotmail.com>
2024-04-29 23:44:00 -0300

    Changed default route to features component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { FeatureTogglerComponent } from './feature-toggler/feature-toggler.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'features', pathMatch: 'full' },
   { path: 'features',  component: FeatureTogglerComponent },
 ];
